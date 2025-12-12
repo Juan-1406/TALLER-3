b016df2c13af04a9c8fc0dacd476f5a29ea2421f
Juan-1406 <juan.alvarado01@alumnos.ucn.cl>
2025-12-12 19:10:41 -0300

    Listar Contenido
    Se implementa la función listar contenido solicitada

--- Sistema.cpp
+++ Sistema.cpp
@@ -13,7 +13,29 @@ void Sistema::crearNodo(int id_padre) {}
 
 void Sistema::eliminar_archivo(int id_archivo, int id_directorio_padre) {}
 
-void Sistema::listar_contenido(int id_directorio) {}
+void Sistema::listar_contenido(int id_directorio) {
+    NodoGrafo* directorio = arbol -> buscar_nodo_grafo(id_directorio);
+    if (!directorio || !directorio -> es_directorio()) {
+        cout << "No es un directorio" << endl;
+        return;
+    }
+
+    cout << "Contenido del directorio" << endl;
+
+    int* hijos = directorio -> lista_hijos();
+    int cantidad = directorio -> cantidadHijos();
+
+    for (int i = 0; i < cantidad; i++) {
+        NodoGrafo* hijo = arbol -> buscar_nodo_grafo(hijos[i]);
+        cout << " - " << hijos[i];
+        if (hijo -> es_directorio()) {
+            cout << " (Directorio)";
+        } else {
+            cout << " (Archivo)";
+        }
+        cout << endl;
+    }
+}
 
 vector<string> Sistema::obtener_rutas_completas(int id_archivo) {}
 
